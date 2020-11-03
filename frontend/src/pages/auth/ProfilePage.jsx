06ce7ec3537fddb9299c325e1deb1bfc2176db4f
drny85 <robertm3lendez@gmail.com>
2020-11-03 16:13:22 -0500

    fix some bugs with profile

--- frontend/src/pages/auth/ProfilePage.jsx
+++ frontend/src/pages/auth/ProfilePage.jsx
@@ -117,150 +117,162 @@ const ProfilePage = () => {
         margin: 'auto',
       }}
     >
-      <Grid container alignContent='center' justify='center' direction='column'>
+      {user && user.isAdmin ? (
         <Grid
-          item
           container
-          justify='space-between'
-          style={{ marginBottom: '15px' }}
+          alignContent='center'
+          justify='center'
+          direction='column'
         >
-          <Typography variant='h5'>Store Profile</Typography>
-          <Grid item>
-            <Button
-              variant='outlined'
-              onClick={() => dispatch(logout())}
-              color='secondary'
-            >
-              Log Out
-            </Button>
-            <Divider />
-          </Grid>
-
-          <Grid item container>
-            <Grid item xs={12} sm={6}>
-              <Paper style={{ width: '100%' }}>
-                <Form style={{ width: '95%' }} onSubmit={updateUserInfo}>
-                  <Controls.Input
-                    name='name'
-                    error={errors.name}
-                    value={values.name}
-                    inputProps={{ style: { textTransform: 'capitalize' } }}
-                    onChange={handleInputChange}
-                    label='Business Name'
-                  />
-                  <Controls.Input
-                    name='address'
-                    error={errors.address}
-                    value={values.address}
-                    inputProps={{ style: { textTransform: 'capitalize' } }}
-                    onChange={handleInputChange}
-                    label='Business Address'
-                  />
-                  <Controls.Input
-                    name='apt'
-                    value={values.apt}
-                    inputProps={{ style: { textTransform: 'capitalize' } }}
-                    onChange={handleInputChange}
-                    label='Apt, Floor or Suite'
-                  />
-                  <Controls.Input
-                    name='city'
-                    value={values.city}
-                    inputProps={{ style: { textTransform: 'capitalize' } }}
-                    error={errors.city}
-                    onChange={handleInputChange}
-                    label='City'
-                  />
-
-                  <Controls.Select
-                    name='state'
-                    value={values.state}
-                    error={errors.state}
-                    onChange={handleInputChange}
-                    label='Select a State'
-                    options={states}
-                  />
-                  <Controls.Input
-                    name='zipcode'
-                    value={values.zipcode}
-                    error={errors.zipcode}
-                    onChange={handleInputChange}
-                    label='Zip Code'
-                  />
-                  <Controls.Input
-                    name='email'
-                    value={values.email}
-                    error={errors.email}
-                    onChange={handleInputChange}
-                    label='Business Email Address'
-                  />
-                  <Controls.Input
-                    name='phone'
-                    error={errors.phone}
-                    value={values.phone}
-                    onChange={handleInputChange}
-                    label='Business Phone Number'
-                  />
-                  <Controls.Input
-                    name='storeInfo'
-                    value={values.storeInfo}
-                    onChange={handleInputChange}
-                    label='Business Description'
-                    placeholder='Enter a brief description about your business'
-                  />
-
-                  <FormControlLabel
-                    control={
-                      <Switch
-                        checked={enableUpdate}
-                        onChange={handleEnableUpdate}
-                        name='checked'
-                        color='primary'
-                      />
-                    }
-                    style={{ paddingLeft: '10px' }}
-                    label='Enable Update'
-                  />
-                  <Controls.Button
-                    disabled={!enableUpdate}
-                    text='UPDATE STORE INFO'
-                    type='submit'
-                    fullWidth
-                  />
-                </Form>
-              </Paper>
+          <Grid
+            item
+            container
+            justify='space-between'
+            style={{ marginBottom: '15px' }}
+          >
+            <Typography variant='h5'>Store Profile</Typography>
+            <Grid item>
+              <Button
+                variant='outlined'
+                onClick={() => dispatch(logout())}
+                color='secondary'
+              >
+                Log Out
+              </Button>
+              <Divider />
             </Grid>
-            <Grid item xs={12} sm={6}>
-              <Paper className='store_detail_list'>
-                <h3 style={{ textAlign: 'center', marginBottom: '15px' }}>
-                  Store Details
-                </h3>
-                <p className='capitalize'>
-                  <strong>Business Name:</strong> {user.store.name}
-                </p>
-                <p className='capitalize'>
-                  <strong>Business Address:</strong> {user.store.address.street}{' '}
-                  {user.store.apt}
-                </p>
-                <p className='capitalize'>
-                  <strong>Business Address Line 2:</strong>{' '}
-                  {user.store.address.city}, {user.store.address.state}{' '}
-                  {user.store.address.zipcode}
-                </p>
-                <p className='capitalize'>
-                  <strong>Business Email:</strong> {user.store.email}
-                </p>
-                <p className='capitalize'>
-                  <strong>Business phone:</strong> {user.store.phone}
-                </p>
-                <p className='capitalize'>
-                  <strong>Business Description:</strong> {user.store.storeInfo}
-                </p>
-              </Paper>
+
+            <Grid item container>
+              <Grid item xs={12} sm={6}>
+                <Paper style={{ width: '100%' }}>
+                  <Form style={{ width: '95%' }} onSubmit={updateUserInfo}>
+                    <Controls.Input
+                      name='name'
+                      error={errors.name}
+                      value={values.name}
+                      inputProps={{ style: { textTransform: 'capitalize' } }}
+                      onChange={handleInputChange}
+                      label='Business Name'
+                    />
+                    <Controls.Input
+                      name='address'
+                      error={errors.address}
+                      value={values.address}
+                      inputProps={{ style: { textTransform: 'capitalize' } }}
+                      onChange={handleInputChange}
+                      label='Business Address'
+                    />
+                    <Controls.Input
+                      name='apt'
+                      value={values.apt}
+                      inputProps={{ style: { textTransform: 'capitalize' } }}
+                      onChange={handleInputChange}
+                      label='Apt, Floor or Suite'
+                    />
+                    <Controls.Input
+                      name='city'
+                      value={values.city}
+                      inputProps={{ style: { textTransform: 'capitalize' } }}
+                      error={errors.city}
+                      onChange={handleInputChange}
+                      label='City'
+                    />
+
+                    <Controls.Select
+                      name='state'
+                      value={values.state}
+                      error={errors.state}
+                      onChange={handleInputChange}
+                      label='Select a State'
+                      options={states}
+                    />
+                    <Controls.Input
+                      name='zipcode'
+                      value={values.zipcode}
+                      error={errors.zipcode}
+                      onChange={handleInputChange}
+                      label='Zip Code'
+                    />
+                    <Controls.Input
+                      name='email'
+                      value={values.email}
+                      error={errors.email}
+                      onChange={handleInputChange}
+                      label='Business Email Address'
+                    />
+                    <Controls.Input
+                      name='phone'
+                      error={errors.phone}
+                      value={values.phone}
+                      onChange={handleInputChange}
+                      label='Business Phone Number'
+                    />
+                    <Controls.Input
+                      name='storeInfo'
+                      value={values.storeInfo}
+                      onChange={handleInputChange}
+                      label='Business Description'
+                      placeholder='Enter a brief description about your business'
+                    />
+
+                    <FormControlLabel
+                      control={
+                        <Switch
+                          checked={enableUpdate}
+                          onChange={handleEnableUpdate}
+                          name='checked'
+                          color='primary'
+                        />
+                      }
+                      style={{ paddingLeft: '10px' }}
+                      label='Enable Update'
+                    />
+                    <Controls.Button
+                      disabled={!enableUpdate}
+                      text='UPDATE STORE INFO'
+                      type='submit'
+                      fullWidth
+                    />
+                  </Form>
+                </Paper>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <Paper className='store_detail_list'>
+                  <h3 style={{ textAlign: 'center', marginBottom: '15px' }}>
+                    Store Details
+                  </h3>
+                  <p className='capitalize'>
+                    <strong>Business Name:</strong> {user.store.name}
+                  </p>
+                  <p className='capitalize'>
+                    <strong>Business Address:</strong>{' '}
+                    {user.store.address.street} {user.store.apt}
+                  </p>
+                  <p className='capitalize'>
+                    <strong>Business Address Line 2:</strong>{' '}
+                    {user.store.address.city}, {user.store.address.state}{' '}
+                    {user.store.address.zipcode}
+                  </p>
+                  <p className='capitalize'>
+                    <strong>Business Email:</strong> {user.store.email}
+                  </p>
+                  <p className='capitalize'>
+                    <strong>Business phone:</strong> {user.store.phone}
+                  </p>
+                  <p className='capitalize'>
+                    <strong>Business Description:</strong>{' '}
+                    {user.store.storeInfo}
+                  </p>
+                </Paper>
+              </Grid>
             </Grid>
           </Grid>
         </Grid>
-      </Grid>
+      ) : (
+        <h3 style={{ textAlign: 'center' }}>
+          So far, Profile is only for Admin users
+        </h3>
+      )}
     </div>
   );
 };
