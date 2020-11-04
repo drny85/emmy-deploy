2404faf6de23b3d7534ae582f95bd882c31c2587
drny85 <robertm3lendez@gmail.com>
2020-11-04 11:09:03 -0500

    changed store dev tools

--- frontend/src/store.jsx
+++ frontend/src/store.jsx
@@ -1,23 +1,24 @@
-import { createStore, applyMiddleware, combineReducers, compose } from 'redux'
-import thunk from 'redux-thunk'
-import categoriesReducer from './reduxStore/reducers/categoriesReducer.'
-import ordersReducer from './reduxStore/reducers/ordersReducer'
-import productsReducer from './reduxStore/reducers/productsReducer'
-import cartReducer from './reduxStore/reducers/shoppingCartReducer'
-import userReducer from './reduxStore/reducers/userReducer'
-
-
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+import categoriesReducer from './reduxStore/reducers/categoriesReducer.';
+import ordersReducer from './reduxStore/reducers/ordersReducer';
+import productsReducer from './reduxStore/reducers/productsReducer';
+import cartReducer from './reduxStore/reducers/shoppingCartReducer';
+import userReducer from './reduxStore/reducers/userReducer';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 
 const reducer = combineReducers({
-    productsData: productsReducer,
-    cartData: cartReducer,
-    userData: userReducer,
-    categoriesData: categoriesReducer,
-    ordersData: ordersReducer
-})
+  productsData: productsReducer,
+  cartData: cartReducer,
+  userData: userReducer,
+  categoriesData: categoriesReducer,
+  ordersData: ordersReducer,
+});
 
-
-const store = createStore(reducer, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
-
+const store = createStore(
+  reducer,
+  {},
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 export default store;
